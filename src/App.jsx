458cbd8ea4a1e790da1e97e58d24a415bd2df0c2
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-03-14 01:06:32 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"; 
 
 // Import components
 import Navbar from "./components/Navbar/Navbar";
@@ -14,41 +14,44 @@ import AwardsComponent from "./components/Awards/Awards";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main style={{ paddingTop: "70px" }}>
-          <Routes>
-            {/* Main Route */}
-            <Route
-              path="/"
-              element={
-                <>
-                  <HeroSection />
-                  <section id="event">
-                    <EventsSection />
-                  </section>
-                  <section id="team">
-                    <MeetTheTeamSection />
-                  </section>
-                  <section id="UpcomingEventsSection">
-                    <UpcomingEventsSection />
-                  </section>
-                  <section>
-                    <AwardsComponent />
-                  </section>
-                  <section id="footer">
-                    <Footer />
-                  </section>
-                </>
-              }
-            />
-            {/* 404 Route */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main style={{ paddingTop: "70px" }}>
+            <Routes>
+              {/* Main Route */}
+              <Route
+                path="/"
+                element={
+                  <>
+                    <HeroSection />
+                    <section id="event">
+                      <EventsSection />
+                    </section>
+                    <section id="team">
+                      <MeetTheTeamSection />
+                    </section>
+                    <section id="UpcomingEventsSection">
+                      <UpcomingEventsSection />
+                    </section>
+                    <section>
+                      <AwardsComponent />
+                    </section>
+                    <section id="footer">
+                      <Footer />
+                    </section>
+                  </>
+                }
+              />
+              {/* 404 Route */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
